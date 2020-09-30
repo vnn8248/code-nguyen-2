25588a6650d6ebcdf9be069ca5f104fe2496eeb7
Bibi Westside <vnn8248@gmail.com>
2020-09-30 16:46:07 -0400

    Update styles, copy, add lodash to blog post title

--- cms/api/blog-posts/models/blog-posts.js
+++ cms/api/blog-posts/models/blog-posts.js
@@ -5,10 +5,12 @@ module.exports = {
     beforeCreate: async (data) => {
       if (data.title) {
         data.slug = _.kebabCase(data.title);
+        data.title = _.startCase(data.title);
       }
     },
     beforeUpdate: async (params, data) => {
       data.slug = _.kebabCase(data.title);
+      data.title = _.startCase(data.title);
     }, 
   }, 
 };
