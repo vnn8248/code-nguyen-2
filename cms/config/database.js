5b6c1f9593ae871fe9a0d3c4e63d2585a0df06c7
Bibi Westside <vnn8248@gmail.com>
2020-10-14 18:37:19 -0400

    Update strapi heroku config file

--- cms/config/database.js
+++ cms/config/database.js
@@ -1,22 +1,15 @@
 module.exports = ({ env }) => ({
-  "defaultConnection": 'default',
+  "defaultConnection": "default",
   "connections": {
     "default": {
-      "connector": 'mongoose',
+      "connector": "mongoose",
       "settings": {
-        // host: env('DATABASE_HOST', '127.0.0.1'),
-        // srv: env.bool('DATABASE_SRV', false),
-        // port: env.int('DATABASE_PORT', 27017),
-        // database: env('DATABASE_NAME', 'cms'),
-        // username: env('DATABASE_USERNAME', ''),
-        // password: env('DATABASE_PASSWORD', ''),
-        "uri": process.env.MONGODB_URI
+        "uri": "${process.env.DATABASE_URI}",
+        "database": "${process.env.DATABASE_NAME}"
       },
       "options": {
-        // authenticationDatabase: env('AUTHENTICATION_DATABASE', null),
-        // ssl: env.bool('DATABASE_SSL', false),
         "ssl": true
-      },
-    },
-  },
+      }
+    }
+  }
 });
